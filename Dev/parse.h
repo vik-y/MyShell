475f5deabf939001d0258ed1df5ce2f7b49eab34
viky <vikas.yadav@iiitb.org>
2015-10-05 10:47:06 +0530

    Readme done and code indented properly

--- Dev/parse.h
+++ Dev/parse.h
@@ -6,20 +6,20 @@
 #define FILE_MAX_SIZE 40
 
 struct commandType {
-  char *command;
-  char *VarList[MAX_VAR_NUM];
-  int VarNum;
+	char *command;
+	char *VarList[MAX_VAR_NUM];
+	int VarNum;
 };
 
 /* parsing information structure */
 typedef struct {
-  int   boolInfile;		       /* boolean value - infile specified */
-  int   boolOutfile;		       /* boolean value - outfile specified */
-  int   boolBackground;		       /* run the process in the background? */
-  struct commandType CommArray[PIPE_MAX_NUM];
-  int   pipeNum;
-  char  inFile[FILE_MAX_SIZE];	       /* file to be piped from */
-  char  outFile[FILE_MAX_SIZE];	       /* file to be piped into */
+	int   boolInfile;		       /* boolean value - infile specified */
+	int   boolOutfile;		       /* boolean value - outfile specified */
+	int   boolBackground;		       /* run the process in the background? */
+	struct commandType CommArray[PIPE_MAX_NUM];
+	int   pipeNum;
+	char  inFile[FILE_MAX_SIZE];	       /* file to be piped from */
+	char  outFile[FILE_MAX_SIZE];	       /* file to be piped into */
 } parseInfo;
 
 /* the function prototypes */
